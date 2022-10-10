fbbf00bb9c0407bd582727d4e30ebaba7342c5f2
aktakukac <mihaly.garamvolgyi@gmail.com>
2022-10-10 13:36:34 +0200

    SQL upsert corrected

--- sql_queries.py
+++ sql_queries.py
@@ -59,7 +59,7 @@ artist_table_create = (
 time_table_create = (
     """
     CREATE TABLE IF NOT EXISTS time (
-        start_time timestamp, 
+        start_time timestamp PRIMARY KEY, 
         hour int, 
         day int,
         week int,
@@ -83,7 +83,7 @@ user_table_insert = (
     INSERT INTO users (user_id, first_name, last_name, gender, level) \
     VALUES (%s, %s, %s, %s, %s) \
     ON CONFLICT (user_id) \
-    DO NOTHING;
+    DO UPDATE SET level = EXCLUDED.level;
     """)
 
 song_table_insert = (
@@ -105,7 +105,8 @@ artist_table_insert = (
 time_table_insert = (
     """
     INSERT INTO time (start_time, hour, day, week, month, year, weekday) \
-    VALUES (%s, %s, %s, %s, %s, %s, %s);
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING;
     """)
 
 # FIND SONGS
